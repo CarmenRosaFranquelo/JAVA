7efad9dca6f7520b09e9e42554862acaaa18bd81
CarmenRosaFranquelo <106599721+CarmenRosaFranquelo@users.noreply.github.com>
2023-03-20 12:35:18 +0100

    Add files via upload

---
+++ IntroduccionProgramacion/Tema8_Encapsulacion/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.setNombre("Pablito");
+        persona1.setEdad(7);
+        persona1.setTelefono(643988998);
+        System.out.println("Nombre " + persona1.getNombre() + " edad " + persona1.getEdad() + " teléfono " + persona1.getTelefono());
+        }
+    }
+
---
+++ IntroduccionProgramacion/Tema8_Encapsulacion/Persona.java
@@ -0,0 +1,35 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+    private int telefono;
+
+    public Persona () {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+    public Persona (String nombre, int edad, int telefono) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+
+    public void setNombre (String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setEdad (int edad) {
+        this.edad = edad;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setTelefono (int telefono) {
+        this.telefono = telefono;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+}
